764012f9cc3b9a7cb133c19f79d30ca9440ac635
sifan <v_sfluo@digitalgd.com.cn>
2020-12-30 19:25:25 +0800

    fix: 移除esm 模块，解决vue 不同版本存在的export 不同的问题

---
+++ Gulpfile.js
@@ -0,0 +1,26 @@
+const { src, dest, series } = require('gulp');
+const jsonEditor = require("gulp-json-editor");
+const { Reflect } = require('core-js');
+const { merge } = require('lodash-es');
+
+const foldPath = './';
+
+function package() {
+  return src("package.json")
+    .pipe(jsonEditor(function(json) {
+      const existJson = {
+        scripts: {
+          test: 'echo "Error: no test specified" && exit 1'
+        }
+      };
+      const tmpJson = merge(json, existJson);
+
+      Reflect.deleteProperty(tmpJson, 'devDependencies');
+      Reflect.deleteProperty(tmpJson.dependencies, 'vue');
+      Reflect.deleteProperty(tmpJson.dependencies, 'core-js');
+
+      return tmpJson;
+    })).pipe(dest(foldPath));
+}
+
+exports.default = series(package)
