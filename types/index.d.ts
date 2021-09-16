2b2286e799a8c1912315c1bb232882154a915b60
sifan <luosifan@lizhi.fm>
2021-09-16 11:09:17 +0800

    test: 测试ts 声明

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,25 @@
 // @ts-ignore
 import Vue, { App, PluginFunction } from "vue";
 
+export declare class CountTo {
+  startVal: number;
+  endVal: number;
+  duration: number;
+  autoplay: boolean;
+  decimals: number;
+  decimal: number;
+  separator: number;
+  prefix: number;
+  suffix: number;
+  useEasing: boolean;
+
+  easingFn: () => void;
+
+  start(): void;
+  pause(): void;
+  reset(): void;
+}
+
 // @ts-ignore
 export declare class CountTo extends Vue {
   startVal: number;
@@ -22,6 +41,7 @@ export declare class CountTo extends Vue {
   reset(): void;
 }
 
+//  extends Vue
 export default class {
   static install (app: App): void;
   static install (app: PluginFunction<never>): void;
