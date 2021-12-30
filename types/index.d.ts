43d9c8477c545ca71723a65d78d9f380ed1275ff
sifan <luosifan@lizhi.fm>
2021-12-30 19:57:32 +0800

    fix: 修改ts 定义

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,22 +1,21 @@
 import Vue, { PluginFunction } from "vue";
 
 export declare class CountTo extends Vue {
-  startVal: number;
-  endVal: number;
-  duration: number;
-  autoplay: boolean;
-  decimals: number;
-  decimal: string;
-  separator: string;
-  prefix: string;
-  suffix: string;
-  useEasing: boolean;
+  startVal?: number;
+  endVal?: number;
+  duration?: number;
+  autoplay?: boolean;
+  decimals?: number;
+  decimal?: string;
+  separator?: string;
+  prefix?: string;
+  suffix?: string;
+  useEasing?: boolean;
 
-  easingFn: (progress: number, localStartVal: number, intervalVal: number, localDuration: number) => number;
-
-  start(): void;
-  pause(): void;
-  reset(): void;
+  easingFn?: (progress: number, localStartVal: number, intervalVal: number, localDuration: number) => number;
+  start?: () => void;
+  pause?: () => void;
+  reset?: () => void;
 }
 
 export default class {
